1a1c56e7fb78f49d983f6ed94dfd3ed0620f3a7d
Alina Glagoleva <07alina20@gmail.com>
2026-04-06 17:36:33 +0700

    Update l2.c

--- linters/l2.c
+++ linters/l2.c
@@ -1,48 +1,84 @@
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <locale.h>
+
+struct list {
+    int data;
+    struct list* next;
+};
+
+struct list* new_node(int data) {
+    struct list* node = (struct list*)malloc(sizeof(struct list));
+    if (!node) return NULL;
+    node->data = data;
+    node->next = NULL;
+    return node;
+}
+
+void print_list(struct list* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+    printf("\n");
+}
+
+int get_sum(struct list* head) {
+    int sum = 0;
+    while (head != NULL) {
+        sum += head->data;
+        head = head->next;
+    }
+    return sum;
+}
+
+void free_list(struct list* head) {
+    while (head != NULL) {
+        struct list* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+struct list* find_max(struct list* head) {
+    struct list* max = head;
+    while (head != NULL) {
+        if (head->data > max->data) {  
+            max = head;
+        }
+        head = head->next;
+    }
+    return max;
+}
 
 int main() {
-    setlocale(LC_ALL, "ru_RU.UTF-8");
-    srand(time(NULL));
-    int secret = rand() % 100 + 1;
-    int yourNumber;
-    int attempts = 0;
-    int* history = NULL;  // массив для хранения попыток
-    int maxAttempts = 10;
-    printf("Угадай (1-100)\n\n");
-    printf("У тебя %d попыток\n\n", maxAttempts);
-    while (attempts < maxAttempts) {
-        printf("Попытка %d: ", attempts + 1);
-        scanf("%d", &yourNumber);
-        history = (int*)realloc(history, (attempts + 1) * sizeof(int));
-        // int* newHistory = (int*)realloc(history, (attempts + 1) * sizeof(int));
-        // if (newHistory == NULL) {
-        //     printf("Ошибка выделения памяти\n");
-        //     free(history);
-        //     return 1;
-        // }
-        // history = newHistory;
-        history[attempts] = yourNumber;
-        attempts++;
-        if (yourNumber == secret) {
-            printf("Поздравляю! Ты угадал число за %d попыток\n", attempts);
-            printf("\nТвои попытки: ");
-            for (int i = 0; i < attempts; i++) {
-                printf("%d ", history[i]);
-            }
-            printf("\n");
-            break;
-        } else if (yourNumber < secret) {
-            printf("Загаданное число больше введенного\n\n");
-        } else {
-            printf("Загаданное число меньше введенного\n\n");
-        }
-    }
-    if (yourNumber != secret) {
-        printf("\nТы проиграл! Загаданное число: %d\n", secret);
-    }
+    struct list* list1 = new_node(13);
+    list1->next = new_node(25);
+    list1->next->next = new_node(3);
+    
+    struct list* list2 = NULL;
+    
+    printf("Первый список: ");
+    print_list(list1);
+    
+    printf("Второй список: ");
+    print_list(list2);
+    
+    int sum1 = get_sum(list1);
+    int sum2 = get_sum(list2);
+    printf("Сумма первого: %d\n", sum1);
+    printf("Сумма второго: %d\n", sum2);
+    
+   
+    struct list* max1 = find_max(list1);
+    struct list* max2 = find_max(list2); // разыменование NULL
+    printf("Максимум в первом: %d\n", max1->data);
+    printf("Максимум во втором: %d\n", max2->data);
+    
+    // утечка памяти
+    int* num = (int*)malloc(sizeof(int));
+    *num = 42;
+
+    free_list(list1);
+    free_list(list2);
     return 0;
 }
