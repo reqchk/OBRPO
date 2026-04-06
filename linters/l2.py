7803976fafc9d5747c6b00ce100e044268823113
Alina Glagoleva <07alina20@gmail.com>
2026-04-06 19:27:26 +0700

    Update l2.py

--- linters/l2.py
+++ linters/l2.py
@@ -2,35 +2,27 @@ import sqlite3
 
 
 def login():
-    # Создаем тестовую БД
     conn = sqlite3.connect(":memory:")
     cursor = conn.cursor()
-    cursor.execute("""
-        CREATE TABLE users (
-            id INTEGER PRIMARY KEY,
-            username TEXT,
-            password TEXT
-        )
-    """)
-    # Добавляем тестового пользователя
+    cursor.execute("""CREATE TABLE users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT,
+                    password TEXT ) """)
+    
     cursor.execute(
         "INSERT INTO users (username, password) VALUES (?, ?)",
-        ("alina", "parol")
+        ("Alina", "QwfghoP10***")
     )
     conn.commit()
     username = input("Введите имя пользователя: ")
     password = input("Введите пароль: ")
+    # ' OR 1=1 --
     query = (
         f"SELECT * FROM users "
         f"WHERE username = '{username}' "
         f"AND password = '{password}'"
     )
     cursor.execute(query)
-    # query = ("""SELECT * FROM users
-    #          WHERE username = ?
-    #          AND password = ?"""
-    #          )
-    # cursor.execute(query, (username, password))
     result = cursor.fetchone()
     if result:
         print("Доступ разрешен")
